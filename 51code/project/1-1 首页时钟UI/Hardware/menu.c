8156f7c42f42133d56b152b7f69990db91607b12
panbaoqi <3306236684@qq.com>
2025-11-30 16:24:04 +0800

    第十三周周报

---
+++ 51code/project/1-1 首页时钟UI/Hardware/menu.c
@@ -0,0 +1,69 @@
+#include "stm32f10x.h"                  // Device header
+#include "MyRTC.h"
+#include "OLED.h"
+#include "Key.h"
+#include "LED.h"
+
+uint8_t KeyNum;
+int clkflag = 1;
+
+void Peripheral_Init(void)
+{
+	MyRTC_Init();
+	Key_Init();
+	LED_Init();
+	
+}
+
+void Show_Clock_UI(void)
+{
+	MyRTC_ReadTime();
+	OLED_Printf(0, 0, OLED_6X8, "%d-%d-%d", MyRTC_Time[0], MyRTC_Time[1], MyRTC_Time[2]);
+	OLED_Printf(16, 16, OLED_12X24, "%02d-%02d-%02d", MyRTC_Time[3], MyRTC_Time[4], MyRTC_Time[5]);
+	OLED_ShowString(0, 48, "菜单", OLED_8X16);//这个8是什么含义，搞清楚，（16x16的字符）
+	OLED_ShowString(96, 48, "设置", OLED_8X16);
+	
+}
+
+int First_Page_Clock(void)
+{
+	while(1)
+	{
+		KeyNum = Key_GetNum();
+		if (KeyNum == 1)
+		{
+			clkflag --;
+			if (clkflag <= 0)
+				clkflag = 2;
+		}
+		else if (KeyNum == 2)
+		{
+			clkflag ++;
+			if (clkflag >= 3)
+				clkflag = 1;
+		}
+		else if (KeyNum == 3)
+		{
+			OLED_Clear();
+			OLED_Update();
+			return clkflag;
+		}
+		
+		switch(clkflag)
+		{
+			case 1:
+				Show_Clock_UI();
+				OLED_ReverseArea(0, 48 , 32, 16);
+				OLED_Update();
+				break;
+			case 2:
+				Show_Clock_UI();
+				OLED_ReverseArea(96, 48, 32, 16);
+				OLED_Update();
+				break;
+				
+		}
+		
+	}
+}
+
